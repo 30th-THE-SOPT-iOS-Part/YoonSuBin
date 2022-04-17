6c07ee6c5cab520ae6312f984e8eb728b4a89c68
devxsby <devxsby@gmail.com>
2022-04-17 18:56:31 +0900

    [Style] Extension의 UIColor 파일의 컬러 에셋 코드 변수명 수정 (#3)
    light_blue, sky_blue, dark_grey 세 가지 컬러셋을 수정하였습니다.

--- Instagram-Clone/Instagram-Clone/Global/Extension/UIColor+.swift
+++ Instagram-Clone/Instagram-Clone/Global/Extension/UIColor+.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UIColor {
-    /// Primary ColorSet
-    ///
-    /// Primary color - (149, 200, 248, 100%)
-    class var primary: UIColor? { return UIColor(named: "primary") }
-    /// Secondary color - (55, 151, 239, 100%)
-    class var secondary: UIColor? { return UIColor(named: "secondary") }
-    
+    /// Main ColorSet
+    /// light blue color - (149, 200, 248, 100%)
+    class var light_blue: UIColor? { return UIColor(named: "light_blue") }
+    /// sky blue color- (55, 151, 239, 100%)
+    class var sky_blue: UIColor? { return UIColor(named: "sky_blue") }
+    /// dark grey color- (114, 114, 114, 100%)
+    class var dark_grey: UIColor? { return UIColor(named: "dark_grey") }
 }
