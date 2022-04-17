88dffbd54e407dc5d5f6d3c785557200371b1806
devxsby <devxsby@gmail.com>
2022-04-17 18:21:44 +0900

    [Update] Extension 파일 중 불필요한 코드 삭제 (#3)
    Extension의 UIView와 UITextField 파일 중 사용하지 않는 코드를 삭제했습니다.

--- Instagram-Clone/Instagram-Clone/Global/Extension/UIView+.swift
+++ Instagram-Clone/Instagram-Clone/Global/Extension/UIView+.swift
@@ -8,12 +8,5 @@
 import UIKit
 
 extension UIView {
-    func makeRound(radius: CGFloat = 4) {
-        layer.cornerRadius = radius
-        clipsToBounds = true
-    }
     
-    func addSubviews(_ views: [UIView]) {
-        views.forEach { self.addSubview($0) }
-    }
 }
