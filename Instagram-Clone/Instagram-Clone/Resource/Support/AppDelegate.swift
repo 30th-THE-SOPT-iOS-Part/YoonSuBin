dc88e7e3e35e564bb3a044aff4727dd9a00f0a41
devxsby <devxsby@gmail.com>
2022-05-04 00:53:05 +0900

    [Add] UIInterfaceOrientationMask 세로 방향 고정 (#6)
    AppDelegate 파일에서 UIInterfaceOrientationMask를 세로방향으로 지정했습니다.

--- Instagram-Clone/Instagram-Clone/Resource/Support/AppDelegate.swift
+++ Instagram-Clone/Instagram-Clone/Resource/Support/AppDelegate.swift
@@ -10,7 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            
+            // 세로방향 고정
+            return UIInterfaceOrientationMask.portrait
+        }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
