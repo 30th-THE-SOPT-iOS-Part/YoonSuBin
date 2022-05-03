153d1490723adfcb79eda6014a3f45a4173dc701
devxsby <devxsby@gmail.com>
2022-05-04 01:44:49 +0900

    [Feat] 메인 Home 뷰 컨트롤러 추가 (#6)

---
+++ Instagram-Clone/Instagram-Clone/Source/Screen/TabBar/Home/Controller/HomeViewController.swift
@@ -0,0 +1,115 @@
+//
+//  HomeViewController.swift
+//  Instagram-Clone
+//
+//  Created by subinyoon on 2022/04/17.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let feedModel = HomeFeedDataModel.feedSampleData
+    private let feedTVC = HomeFeedTableViewCell(style: .default, reuseIdentifier: "")
+    
+    private enum SectionLayout: CaseIterable {
+        case story, feed
+    }
+
+    // MARK: - UI Component Part
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    // MARK: - Life Cycle Part
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerNib()
+    }
+    
+    // MARK: - IBAction Part
+    @IBAction func addBtnDidTap(_ sender: Any) {
+        print("add btn did tap")
+    }
+
+    @IBAction func likeBtnDidTap(_ sender: Any) {
+        print("like btn did tap")
+    }
+
+    @IBAction func messageBtnDidTap(_ sender: Any) {
+        print("message btn did tap")
+    }
+    
+    // MARK: - TableViewCell registerNib Part
+    private func registerNib() {
+        
+        let storyNib = UINib(nibName: HomeStoryTableViewCell.identifier, bundle: nil)
+        homeTableView.register(storyNib, forCellReuseIdentifier: HomeStoryTableViewCell.identifier)
+        
+        let feedNib = UINib(nibName: HomeFeedTableViewCell.identifier, bundle: nil)
+        homeTableView.register(feedNib, forCellReuseIdentifier: HomeFeedTableViewCell.identifier)
+        
+        homeTableView.rowHeight = UITableView.automaticDimension
+        homeTableView.estimatedRowHeight = 488
+        
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+    }
+}
+
+
+// MARK: - UITableView Delegate
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableView DataSource
+extension HomeViewController: UITableViewDataSource {
+    
+    /// 테이블 뷰의 전체 section수를 지정하는는 메서드
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SectionLayout.allCases.count
+    }
+    
+    /// 테이블 뷰의 section의 index를 가지고 해당 sections에 포함된 셀 수를 알려주는 메서드
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        switch SectionLayout.allCases[section] {
+        case .story:
+            return 1
+        case .feed:
+            return HomeFeedDataModel.feedSampleData.count
+        }
+    }
+    
+    /// 테이블 뷰에서 특정 index에 있는 셀을 알려주는 메서드
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch SectionLayout.allCases[indexPath.section] {
+        case .story:
+            guard let storyCell = homeTableView.dequeueReusableCell(withIdentifier: HomeStoryTableViewCell.identifier, for: indexPath) as? HomeStoryTableViewCell else { return UITableViewCell() }
+            return storyCell
+            
+        case .feed:
+            guard let feedCell = homeTableView.dequeueReusableCell(withIdentifier: HomeFeedTableViewCell.identifier, for: indexPath) as? HomeFeedTableViewCell else { return UITableViewCell() }
+            
+            feedCell.setDataModel(feedData: HomeFeedDataModel.feedSampleData[indexPath.row])
+            
+            feedCell.likesButtonEvent = {
+                /// 클로저 좋아요 카운트 이벤트
+                if feedCell.likeButton.isSelected == true {
+                    print("클로저 좋아요 설정")
+                    let count = feedCell.likesCount + 1
+                    feedCell.likeLabel.text = "좋아요 \(count)개"
+                } else {
+                    print("클로저 좋아요 해제")
+                    let count = feedCell.likesCount + 0
+                    feedCell.likeLabel.text = "좋아요 \(count)개"
+                }
+            }
+            return feedCell
+        }
+    }
+}
