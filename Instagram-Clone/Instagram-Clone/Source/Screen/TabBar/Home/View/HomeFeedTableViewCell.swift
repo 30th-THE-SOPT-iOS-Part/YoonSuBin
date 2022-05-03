ae6d24a97314b894820f4d8f375c881ab7661caf
devxsby <devxsby@gmail.com>
2022-05-04 01:43:57 +0900

    [Feat] Feed 테이블 뷰 셀 추가 (#6)

---
+++ Instagram-Clone/Instagram-Clone/Source/Screen/TabBar/Home/View/HomeFeedTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  HomeTableViewCell.swift
+//  Instagram-Clone
+//
+//  Created by subinyoon on 2022/04/28.
+//
+
+import UIKit
+
+class HomeFeedTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    static let identifier = "HomeFeedTableViewCell"
+    public var likesButtonEvent : (() -> ())? // 버튼 이벤트 클로저로 구현
+    public var likesCount: Int!
+    
+    // MARK: - UI Component Part
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileUsernameLabel: UILabel!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    
+    @IBOutlet weak var photoPageControl: UIPageControl!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    @IBOutlet weak var viewAllCommentsButton: UIButton!
+
+    // MARK: - awakeFromNib
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    // MARK: - IBAction Part
+    @IBAction func photoPageChanged(_ sender: Any) {
+
+    }
+    
+    @IBAction func likeBtnDidTap(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        likesButtonEvent?() // 좋아요 이벤트 클로저
+    }
+    
+    // MARK: - Feed Data Model과 연결
+    func setDataModel(feedData: HomeFeedDataModel) {
+        
+        likesCount = feedData.likes // likes값 넘기기
+        
+        profileImageView.image = UIImage(named: feedData.profileImage)
+        profileUsernameLabel.text = feedData.profileUsername
+        
+        // + 페이지 컨트롤 구현하기
+        photoImageView.image = UIImage(named: feedData.photo)
+        likeLabel.text = "좋아요 \(feedData.likes)개"
+        
+        usernameLabel.text = feedData.username
+        captionLabel.text = feedData.caption
+        
+        viewAllCommentsButton.setTitle("댓글 \(feedData.comments)개 모두 보기", for: .normal)
+    }
+}
