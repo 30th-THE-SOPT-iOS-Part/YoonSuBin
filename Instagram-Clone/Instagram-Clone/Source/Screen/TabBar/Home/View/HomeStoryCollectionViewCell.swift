1f38e4b59a41607240d06f59727b776e14fa03b6
devxsby <devxsby@gmail.com>
2022-05-04 23:11:39 +0900

    [Chore] customUI 스토리보드에서 구현 (#6)
    스토리보드에서 프로필 이미지를 둥글게 하고 배경색을 검정으로 부여합니다.

--- Instagram-Clone/Instagram-Clone/Source/Screen/TabBar/Home/View/HomeStoryCollectionViewCell.swift
+++ Instagram-Clone/Instagram-Clone/Source/Screen/TabBar/Home/View/HomeStoryCollectionViewCell.swift
@@ -19,20 +19,11 @@ class HomeStoryCollectionViewCell: UICollectionViewCell {
     // MARK: - awakeFromNib
     override func awakeFromNib() {
         super.awakeFromNib()
-        setUI()
     }
     
     // MARK: - Story Data Model과 연결
     func setDataModel(storyData: HomeStoryDataModel) {
         profileImageView.image = UIImage(named: storyData.profileImage)
         profileUsernameLabel.text = storyData.profileUsername
-    }
-    
-    // MARK: - Custom UI
-    func setUI() {
-        /// 스토리 프로필 이미지를 둥글게 하고 검은색 배경을 줍니다.
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
-        profileImageView.layer.cornerRadius = frame.height / 3
-        profileImageView.backgroundColor = .black
     }
 }
