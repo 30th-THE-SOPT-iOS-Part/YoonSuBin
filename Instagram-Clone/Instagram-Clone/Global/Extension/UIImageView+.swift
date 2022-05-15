bee56db4f8ae6b0b5a2ecf5bcd616ddc04c5dd43
devxsby <devxsby@gmail.com>
2022-05-16 00:36:17 +0900

    [Chore] 주석 삭제 (#9)

--- Instagram-Clone/Instagram-Clone/Global/Extension/UIImageView+.swift
+++ Instagram-Clone/Instagram-Clone/Global/Extension/UIImageView+.swift
@@ -11,15 +11,15 @@ import Kingfisher
 extension UIImageView {
     func setImage(with urlString: String) {
           let cache = ImageCache.default
-          cache.retrieveImage(forKey: urlString, options: nil) { result in // 캐시에서 키를 통해 이미지를 가져온다.
+          cache.retrieveImage(forKey: urlString, options: nil) { result in
               switch result {
               case .success(let value):
-                  if let image = value.image { // 만약 캐시에 이미지가 존재한다면
-                      self.image = image // 바로 이미지를 셋한다.
+                  if let image = value.image {
+                      self.image = image
                   } else {
                       guard let url = URL(string: urlString) else { return }
-                      let resource = ImageResource(downloadURL: url, cacheKey: urlString) // URL로부터 이미지를 다운받고 String 타입의 URL을 캐시키로 지정하고
-                      self.kf.setImage(with: resource) // 이미지를 셋한다.
+                      let resource = ImageResource(downloadURL: url, cacheKey: urlString)
+                      self.kf.setImage(with: resource)
                   }
               case .failure(let error):
                   print(error)
